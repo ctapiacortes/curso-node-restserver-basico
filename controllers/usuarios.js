0372b387b0d335763ed1803c9f5b87a342ede26c
Cristian Tapia <cristian.tc89@gmail.com>
2021-06-22 23:52:00 -0400

    Subiendo backend

---
+++ controllers/usuarios.js
@@ -0,0 +1,45 @@
+const { response, request } = require('express')
+
+const usuariosGet = (req = request, res = response) => {
+
+    const params = req.query
+        //const { cliente = 'Cristian'} = req.query
+
+    res.json({
+        msg: 'get API - controlador',
+        params
+    })
+}
+
+const usuariosPut = (req, res) => {
+
+    const id = req.params.id;
+
+    res.json({
+        msg: 'put API - controlador',
+        id
+    })
+}
+
+const usuariosPost = (req, res) => {
+
+    const body = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        body
+    })
+}
+
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: 'delete API - controlador'
+    })
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete
+}
